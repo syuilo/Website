dadea5282d3844edd98dc1de6d7f9ee8fcd2d014
Afuafu <noodle117117@yahoo.co.jp>
2015-03-12 22:54:12 +0900

    Fix header

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
 $(function() {
-
+	$(window).on('load scroll', function() {
+		if ($(window).scrollTop() > $('header[role="banner"]').height()) {
+			$('header[role="banner"]').addClass('fix');
+		} else {
+			$('header[role="banner"]').removeClass('fix');
+		}
+	});
 });
